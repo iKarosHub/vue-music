61157b67a786532dea8c78fbea6d54275cbc0e30
iKaros <iKaroshub@163.com>
2020-12-03 16:16:59 +0800

    new

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: () => import('@/views/Home')
+  },
+  {
+    path: '/f',
+    name: 'find',
+    component: () => import('@/views/find/H')
   }
 ]
 
